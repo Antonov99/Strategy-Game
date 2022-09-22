d66e610dee2125521e4c840d39de0c273daa8b52
Aleksey <lexa.antonov-alex@yandex.ru>
2022-09-22 16:34:53 +0300

    Следование

--- spy eye/Assets/skripti/WarController.cs
+++ spy eye/Assets/skripti/WarController.cs
@@ -5,28 +5,50 @@ using UnityEngine.AI;
 
 public class WarController : MonoBehaviour
 {
+    public Transform enemy;
     private NavMeshAgent v1_agent;
     public Vector3 _targ;
-    public GameObject _koster;
+    public Transform trans;
+    public Transform _koster;
     public float Speed = 0.0f;
+    public float radiusVid=10;
+    public float radiusAtaki=2;
+    public float distance; 
+    Animator v1_animator;
 
     public void Awake()
     {
-        _targ = _koster.transform.position;
+        trans=this.transform;
+        _targ = _koster.position;
         v1_agent = GetComponent<NavMeshAgent>();
         v1_agent.speed = Speed;
-
+        v1_animator = GetComponent<Animator>();
     }
 
     private void Start()
-    {
-        SledKoster();
+    {       
+        
     }
 
-
-    public void SledKoster()
+    private void Update()
     {
-
-        v1_agent.SetDestination(_targ);
+        distance = Vector3.Distance(trans.position, enemy.transform.position);
+        Debug.Log(distance);
+        if (distance < radiusVid && distance>radiusAtaki)
+        {
+            v1_animator.SetBool("vizhuvraga", true);
+            v1_agent.SetDestination(enemy.transform.position);
+        }
+        else if (distance < radiusAtaki)
+        {
+            v1_animator.SetBool("vizhuvraga", false);
+            v1_animator.SetBool("byuvraga", true);
+            v1_agent.enabled = false;
+        }
+        else
+        {
+            v1_agent.SetDestination(_targ);
+        }
     }
+
 }
