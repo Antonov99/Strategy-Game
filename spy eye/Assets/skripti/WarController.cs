b6e31d3d971c3f0173e4420ee96bf19525a9d4c7
Aleksey <lexa.antonov-alex@yandex.ru>
2022-10-04 17:14:39 +0300

    Кнопка атаки(не работает)

--- spy eye/Assets/skripti/WarController.cs
+++ spy eye/Assets/skripti/WarController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class WarController : MonoBehaviour
 {
@@ -10,17 +11,20 @@ public class WarController : MonoBehaviour
     private Vector3 _targ;
     private Transform trans;
     public Transform _Target;
+    public Transform targetAttack;
     public float radiusVid = 10;
     public float radiusAtaki = 0.5f;
     private float distanceToEnemy;
     private float distanceToKoster;
     Animator v1_animator;
 
+    public bool Attack;
+
     public void Awake()
     {
         enemy = GameObject.FindGameObjectWithTag("enemy");
         trans = this.transform;
-        _targ = new Vector3(_Target.position.x+Random.Range(-5,5), _Target.position.y, _Target.position.z+Random.Range(-2,2));
+        _targ = new Vector3(_Target.position.x + Random.Range(-5, 5), _Target.position.y, _Target.position.z + Random.Range(-2, 2));
         v1_agent = GetComponent<NavMeshAgent>();
         v1_animator = GetComponent<Animator>();
     }
@@ -47,25 +51,48 @@ public class WarController : MonoBehaviour
             v1_animator.SetBool("walk", false);
             v1_animator.SetBool("attack", true);
         }
-        else
+        else if (Attack==true)
         {
-            distanceToKoster = Vector3.Distance(trans.position, _targ);
-            if (distanceToKoster > v1_agent.stoppingDistance)
-            {
-                v1_agent.speed = 1;
-                v1_animator.SetBool("run", false);
-                v1_animator.SetBool("attack", false);
-                v1_animator.SetBool("walk", true);
-                v1_agent.SetDestination(_targ);
-            }
-            else
-            {
-                v1_animator.SetBool("run", false);
-                v1_animator.SetBool("attack", false);
+                v1_agent.speed = 3;
                 v1_animator.SetBool("walk", false);
-                trans.rotation = Quaternion.Euler(0,0,0);
-            }
+                v1_animator.SetBool("attack", false);
+                v1_animator.SetBool("run", true);
+                v1_agent.SetDestination(targetAttack.transform.position);
+
         }
+        else 
+        {
+                distanceToKoster = Vector3.Distance(trans.position, _targ);
+                if (distanceToKoster > v1_agent.stoppingDistance)
+                {
+                    v1_agent.speed = 1;
+                    v1_animator.SetBool("run", false);
+                    v1_animator.SetBool("attack", false);
+                    v1_animator.SetBool("walk", true);
+                    v1_agent.SetDestination(_targ);
+                }
+                else
+                {
+                    v1_animator.SetBool("run", false);
+                    v1_animator.SetBool("attack", false);
+                    v1_animator.SetBool("walk", false);
+                    trans.rotation = Quaternion.Euler(0, 0, 0);
+                }
+        }
+        
     }
 
+    public void attack()
+    {
+        if (Attack)
+        {
+            Attack = false;
+            Debug.Log(Attack);
+        }
+        else
+        {
+            Attack = true;
+            Debug.Log(Attack);
+        }
+    }
 }
